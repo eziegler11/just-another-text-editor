4987b59791806ab71c4c37b49baeae4fc630eee5
erichziegler <erich.ziegler11@gmail.com>
2023-05-17 17:27:36 -0600

    Moved initial code into repo directory

---
+++ client/src/js/database.js
@@ -0,0 +1,37 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => console.error('putDb not implemented');
+
+// DONE: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  const contactDb = await openDB('jate', 1);
+
+  const tx = contactDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+
+// console.error('getDb not implemented');
+
+initdb();
