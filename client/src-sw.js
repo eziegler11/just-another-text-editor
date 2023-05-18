00f34aeb421eb7e014e6b1fa6323a0186649b883
erichziegler <erich.ziegler11@gmail.com>
2023-05-17 18:20:58 -0600

    JATE is working, manifest displays, going to upload to Heroku

--- client/src-sw.js
+++ client/src-sw.js
@@ -8,23 +8,33 @@ const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-  ],
+	cacheName: 'page-cache',
+	plugins: [
+		new CacheableResponsePlugin({
+			statuses: [0, 200],
+		}),
+		new ExpirationPlugin({
+			maxAgeSeconds: 30 * 24 * 60 * 60,
+		}),
+	],
 });
 
 warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
+	urls: ['/index.html', '/'],
+	strategy: pageCache,
 });
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+	({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+	new StaleWhileRevalidate({
+		cacheName: 'asset-cache',
+		plugins: [
+			new CacheableResponsePlugin({
+				statuses: [0, 200],
+			}),
+		],
+	})
+);
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,15 +13,28 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+
+  const editorDb = await openDB('jate', 1);
+
+  const tx = editorDb.transaction('contact', 'readwrite');
+
+  const store = tx.objectStore('contact');
+
+  const request = store.add(content);
+
+  const result = await request;
+  console.log('Data put to the database', result);
+};
 
 // DONE: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
 
-  const contactDb = await openDB('jate', 1);
+  const editorDb = await openDB('jate', 1);
 
-  const tx = contactDb.transaction('jate', 'readonly');
+  const tx = editorDb.transaction('jate', 'readonly');
 
   const store = tx.objectStore('jate');
 
