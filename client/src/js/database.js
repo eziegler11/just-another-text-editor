fbd4b874e6d32070ca3329f1fa384bd555bdf096
erichziegler <erich.ziegler11@gmail.com>
2023-05-17 19:21:24 -0600

    Changed another variable in database file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -22,7 +22,7 @@ export const putDb = async (content) => {
 
   const store = tx.objectStore('jate');
 
-  const request = store.add(content);
+  const request = store.put({id: 1, value: content});
 
   const result = await request;
   console.log('Data put to the database', result);
