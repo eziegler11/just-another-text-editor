163a8df3b18895f452909bd7846e0e82510a02de
erichziegler <erich.ziegler11@gmail.com>
2023-05-21 20:42:10 -0600

    Got database working, service worker and save data on refresh.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,48 +1,49 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
+	openDB('jate', 1, {
+		upgrade(db) {
+			if (db.objectStoreNames.contains('jate')) {
+				console.log('jate database already exists');
+				return;
+			}
+			db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+			console.log('jate database created');
+		},
+	});
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
+	// console.error('putDb not implemented');
 
-  const jateDB = await openDB('jate', 1);
+	const jateDB = await openDB('jate', 1);
 
-  const tx = jateDB.transaction('jate', 'readwrite');
+	const tx = jateDB.transaction('jate', 'readwrite');
 
-  const store = tx.objectStore('jate');
+	const store = tx.objectStore('jate');
 
-  const request = store.put({id: 1, value: content});
+	const request = store.put({ id: 1, value: content });
 
-  const result = await request;
-  console.log('Data put to the database', result);
+	const result = await request;
+	console.log('Data put to the database', result);
 };
 
 // DONE: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log('GET from the database');
+	console.log('GET from the database');
 
-  const jateDB = await openDB('jate', 1);
+	const jateDB = await openDB('jate', 1);
 
-  const tx = jateDB.transaction('jate', 'readonly');
+	const tx = jateDB.transaction('jate', 'readonly');
 
-  const store = tx.objectStore('jate');
+	const store = tx.objectStore('jate');
 
-  const request = store.getAll();
+	//const request = store.getAll();
+	const request = store.get(1);
 
-  const result = await request;
-  console.log('result.value', result);
-  return result;
+	const result = await request;
+	console.log('result.value', result);
+	return result.value;
 };
 
 // console.error('getDb not implemented');
